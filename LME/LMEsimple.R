6b8f6943dc109549ec010792bfdfded17bd156e3
Joanne.Li <xlhy123@joannelidembp.sph.ad.jhsph.edu>
2015-12-17 16:05:25 -0500

    test

---
+++ LME/LMEsimple.R
@@ -0,0 +1,11 @@
+filename ="LMEsimple.html"
+swirlfile <- .path_to_file("swirl_course", "LME/html", filename)
+tf <- tempfile()
+dir.create(tf)
+htmlfile = file.path(tf,filename)
+file.copy(swirlfile, htmlfile)
+if(rstudioapi::isAvailable()){
+   rstudioapi::viewer(htmlfile)
+}else{
+   browseURL(htmlfile)
+}
---
+++ LME/readdata.R
@@ -0,0 +1,2 @@
+real.data = read.table("http://www.unt.edu/rss/class/Jon/R_SC/Module9/lmm.data.txt", 
+                       header = TRUE, sep = ",", na.strings = "NA", dec = ".", strip.white = TRUE)
